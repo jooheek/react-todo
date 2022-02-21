e10991a5f56fe6c026d6fac0aab5fd325b6b233c
jooheek <kimjoohee713@gmail.com>
2022-02-21 23:08:41 +0900

    refactoring categoryState

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -7,24 +7,21 @@ export interface IToDo {
   category: 'TO_DO' | 'DOING' | 'DONE';
 }
 
+export const categoryState = atom({
+  key: 'category',
+  default: 'TO_DO',
+});
+
 export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
 });
 
-//selector를 사용하는 이유 : atom의 데이터를 변형시켜서 output값으로 출력할 수 있다.
-
-//selector는 state를 가져다가 어떤것을 return 한다.
-//derived state라고도 힘
-//get에서 return 하는 값이 toDoSelector return 값이다. get 함수가 atom의 값을 받게 해준다.
 export const toDoSelector = selector({
   key: 'toDoSelector',
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((todo) => todo.category === 'TO_DO'),
-      toDos.filter((todo) => todo.category === 'DOING'),
-      toDos.filter((todo) => todo.category === 'DONE'),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((todo) => todo.category === category);
   },
 });
